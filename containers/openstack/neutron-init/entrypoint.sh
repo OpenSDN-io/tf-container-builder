2bf609537a7bc24f3650b00d5980f4e0e78e3015
ospanovalen <ospanov.a@phystech.edu>
2024-08-27 14:58:51 +0300

    remove python2 support from container-builder
    Change-Id: I2f0414983f1d23e92d609a80f56a2e9f15041cc4
    Depends-On: Ie989858f08ea62c6de3da5b119c96de0b3aa8874
    Depends-On: I6613309fdd713f421d08cafe1229fb78b1d3ecce
    Signed-off-by: ospanovalen <ospanov.a@phystech.edu>

--- containers/openstack/neutron-init/entrypoint.sh
+++ containers/openstack/neutron-init/entrypoint.sh
@@ -20,35 +20,7 @@ function copy_sources() {
   done
 }
 
-mkdir -p /opt/plugin/site-packages /opt/plugin/python3/site-packages
+mkdir -p /opt/plugin/site-packages
 
 # python3
-cp -rf /opt/contrail_python3/site-packages/* /opt/plugin/python3/site-packages/
-
-# python2
 cp -rf /opt/contrail/site-packages/* /opt/plugin/site-packages/
-
-# install appropriate version of python-neutron-lbaas based on OPENSTACK_VERSION
-# do not fail grep if packages are absent
-pkg=""
-pkg_all=""
-
-if [[ -d "/opt/packages/" ]]; then
-  pkg_all=$(ls -1 /opt/packages/)
-  pkg=$(echo "$pkg_all" | grep python-neutron-lbaas-$OPENSTACK_VERSION || /bin/true)
-else
-  echo "INFO: directory /opt/packages/ couldn't be found"
-fi
-
-if [[ -z "$pkg" ]]; then
-  # rhel case
-  echo -e "INFO: package couldn't be found for this version: $pkg_version. Available packages:\n$pkg_all"
-else
-  mkdir -p /tmp/lbaas
-  pushd /tmp/lbaas
-  # rpm install can't be used here cause this container can be run under non-root user
-  rpm2cpio /opt/packages/$pkg | cpio -idmv
-  copy_sources usr/lib/python2.7/site-packages neutron_lbaas
-  popd
-  rm -rf /tmp/lbaas
-fi
