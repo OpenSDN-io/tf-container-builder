adef424a74b776442926916e81bc0e9b9a472a0a
Andrey Pavlov <andrey-mp@yandex.ru>
2025-09-04 16:21:29 +0300

    add /etc mounts from host into k8s manifest node-init
    Change-Id: I6736b920bf78264388a94e3fe4a479f59308ab73

--- containers/node-init/entrypoint.sh
+++ containers/node-init/entrypoint.sh
@@ -6,15 +6,21 @@ export SERVICE_NAME=node-init
 ret=0
 
 /sysctl-init.sh || ret=1
+echo "INFO: sysctl-init ret code = $ret"
 
 /opensdn-status-init.sh || ret=1
+echo "INFO: opensdn-status-init ret code = $ret"
 
 /opensdn-tools-init.sh || ret=1
+echo "INFO: opensdn-tools-init ret code = $ret"
 
 /certs-init.sh || ret=1
+echo "INFO: certs-init ret code = $ret"
 
 /files-init.sh || ret=1
+echo "INFO: files-init ret code = $ret"
 
 /firewall.sh || ret=1
+echo "INFO: firewall ret code = $ret"
 
 exit $ret
